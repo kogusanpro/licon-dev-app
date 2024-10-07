f01607717ef909561cb86bbdf82d737b22cca10c
takashi-kogure-geniee <takashi.kogure@geniee.co.jp>
2024-10-07 20:58:41 +0900

    add config

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config.from_object('testapp.config')
+app.config.from_object('testapp.config')
 
 db = SQLAlchemy(app)
